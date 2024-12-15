a2c224ac3e2366265e14d2a0e4e8c137f92831b4
nachodev-ui <ig.cisternas@duocuc.cl>
2024-12-14 21:33:46 -0300

    feat(cat entity): Se crea la tabla Cat con sus atributos y relación

---
+++ src/cats/entities/cat.entity.ts
@@ -0,0 +1,22 @@
+import { Breed } from 'src/breeds/entities/breed.entity'
+import { Column, DeleteDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity()
+export class Cat {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    name: string
+
+    @Column()
+    age: number
+
+    @DeleteDateColumn()
+    deletedAt: Date
+
+    @ManyToOne(() => Breed, (breed) => breed.id, {
+      eager: true // Para que se cargue el gato con la raza
+    })
+    breed: Breed
+}
